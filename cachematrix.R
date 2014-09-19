35ec46c9a3d9a81b11fcf8b4e54f28321a690c23
rajesh5738 <krajesh1@bus.alliance.edu.in>
2014-09-19 21:43:52 +0530

    Commented Line Removed

--- cachematrix.R
+++ cachematrix.R
@@ -2,17 +2,6 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
 makeCacheMatrix <- function( mat = matrix() ) {
   inverse <- NULL
   setMat <- function( matrix ) {
